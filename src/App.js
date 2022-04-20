74ec5a8058aaa4f1601640e7fc4e64163889b052
schm00g <hk8igureu@relay.firefox.com>
2022-04-20 17:27:30 +0200

    object array snippet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import * as React from 'react';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>Hacker Stories</h1>
+
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function(item){
+          return <li>{item.title}</li>
+        })}
+      </ul>
+
     </div> 
   );
 }
 
 export default App;
+
+// page: 21
