5960440963f8a74d4a95cb7f2ed2b1367c7e6c4b
schm00g <hk8igureu@relay.firefox.com>
2022-04-21 11:08:28 +0200

    remove blocks

--- src/App.js
+++ src/App.js
@@ -17,48 +17,39 @@ const list = [
     objectID: 1,
 }, ];
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+  </div> 
+);
 
-      <Search />
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
 
-      <hr />
-
-      <List />
-    </div> 
-  );
-}
-
-const Search = () => {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  )
-}
-
-const List = () => {
-  return (
-    <div>
-      <ul>
-        {list.map(function(item){
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>gs
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  )
-}
+const List = () => (
+  <div>
+    <ul>
+      {list.map(function(item){
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>gs
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+);
 
 export default App;
