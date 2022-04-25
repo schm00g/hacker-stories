cc97a04061e50f7885ad0c34b1d4e8319e5fa07e
schm00g <hk8igureu@relay.firefox.com>
2022-04-25 15:27:15 +0200

    lifting state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import * as React from 'react';
 
 const App = () => {
-
-  const handleSearch = (event) => {
-    console.log(event.target.value);
-  }
-
   const stories = [
     {
       title: 'React',
@@ -14,43 +9,40 @@ const App = () => {
       num_comments: 3,
       points: 4,
       objectID: 0,
-  }, {
+    }, {
       title: 'Redux',
       url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
-  }, ];
+    }, ];
+    
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
 
   return (
     <div>
     <h1>Hacker Stories</h1>
     <Search onSearch={handleSearch} />
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
     <hr />
     <List list={stories}/>
     </div> 
   )
 };
 
-const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  }
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
-}
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch}/>
+  </div>
+);
 
 const List = (props) => {
   return (
@@ -77,4 +69,4 @@ const Item = (props) => (
 
 export default App;
 
-// page 49
+// page 51
