0b8b93a86cd4233af3627e84f6d14e05090f34a4
schm00g <hk8igureu@relay.firefox.com>
2022-04-29 08:22:12 +0100

    basic unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe('something truthy and falsy', () => {
+  test('true to be true', () => {
+    expect(true).toBe(true);
+  });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test('expect false to be false', () => {
+    expect(false).toBe(false);
+  });
 });
